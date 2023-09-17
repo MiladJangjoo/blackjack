299618fc5904a656997a79cea304355abcfbfae0
miladjangjoo <miladjangjooo@gmail.com>
2023-09-17 17:10:09 -0500

    hii

--- blackjack.py
+++ blackjack.py
@@ -24,19 +24,22 @@ class BlackJack():
         return self.startingDeck
     
     def player (self):
-
-        for i in self.startingDeck:
-            if len(self.playerHand) < 3:
-                self.playerHand.append(i)
-                self.startingDeck.remove(i)
+        one = self.startingDeck[0]
+        two = self.startingDeck[1]
+        self.playerHand.append(one)
+        self.playerHand.append(two)
+        self.startingDeck.remove(one)
+        self.startingDeck.remove(two)
         print (self.startingDeck)
         return f'player hand is {self.playerHand[0]} and {self.playerHand[1]}'
     
     def dealer (self):
-        for i in self.startingDeck:
-            if len(self.dealerHand) < 3:
-                self.dealerHand.append(i)
-                self.startingDeck.remove(i)
+        one = self.startingDeck[0]
+        two = self.startingDeck[1]
+        self.dealerHand.append(one)
+        self.dealerHand.append(two)
+        self.startingDeck.remove(one)
+        self.startingDeck.remove(two)
         print (self.startingDeck)
         return f'dealer hand is {self.dealerHand[0]} and XXXXXX'
     
